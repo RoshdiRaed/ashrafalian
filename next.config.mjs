d7b5ffa8701ba9c3b8c167c632c12ee6639bedb2
roshdiraed <roshdi013@gmail.com>
2025-01-01 13:00:34 +0200

    speedv1.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    images: {
+      domains: ['https://ashrafalian-v1.netlify.app/'],
+      formats: ['image/avif', 'image/webp'],
+    },
+    compress: true,
+    reactStrictMode: true,
+  };
+  
+
 export default nextConfig;
